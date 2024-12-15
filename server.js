faea5951419be27c38ff68640fc57196bcc3070e
tarekul <rahin6810@gmail.com>
2024-12-14 19:12:37 -0500

    add npm rebuild sqlite3

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 // Middleware
 app.use(cors());
