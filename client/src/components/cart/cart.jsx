6c9a1f3f3206ad683012b6e02cc569d73dddd59d
tarekul <rahin6810@gmail.com>
2024-12-16 03:42:19 -0500

    add total price to cart and create checkout directory

--- client/src/components/cart/cart.jsx
+++ client/src/components/cart/cart.jsx
@@ -3,6 +3,12 @@ import CartItem from "../cart-item/cart-item";
 import "./cart.styles.css";
 
 const Cart = ({ cart, addToCart, removeFromCart, isDropdownOpen, setIsDropdownOpen }) => {
+    const totalPrice = cart.reduce(
+        (totalPrice, cartItem) => (totalPrice += cartItem.quantity * cartItem.item.price),
+        0
+    );
+
+    const hasItems = cart.length > 0 ? 'has-items' : '';
 
     const toggleDropdown = () => {
         if (!isDropdownOpen) {
@@ -28,9 +34,10 @@ const Cart = ({ cart, addToCart, removeFromCart, isDropdownOpen, setIsDropdownOp
 
             {/* Dropdown Menu */}
             {isDropdownOpen && (
-                <div className="cart-dropdown {cart.length > 0 ? has-items : ''}">
+                <div className={`cart-dropdown ${hasItems}`}>
                     <h2>Your Cart</h2>
                     <button className="close-button" onClick={toggleDropdown}>X</button>
+                    <p className="total-price">Total: ${totalPrice.toFixed(2)}</p>
                     {cart.length > 0 ? (
                         <div className="cart-items">
                             {cart.map((cartItem) => (
