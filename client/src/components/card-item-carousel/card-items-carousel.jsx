01196bb8b34b6f1f755edf77bb177bad8134829c
tarekul <rahin6810@gmail.com>
2024-12-20 18:41:36 -0500

    add grid for items less than 4

--- client/src/components/card-item-carousel/card-items-carousel.jsx
+++ client/src/components/card-item-carousel/card-items-carousel.jsx
@@ -3,10 +3,11 @@ import './card-items-carousel.styles.css';
 import CardItem from '../card-item/card-item';
 
 const CardItemsCarousel = ({ inventory, editCart, isDropdownOpen, category }) => {
+    const isGridLayout = inventory.length < 4;
     return (
         <div className={`card-items-carousel ${isDropdownOpen ? 'hide' : ''}`}>
             <h5 className="category-title">{category}</h5>
-            <div className="carousel-items">
+            <div className={`items-list ${isGridLayout ? 'grid' : 'carousel'}`}>
                 {inventory.map(item => (
                     <CardItem key={item.id} item={item} editCart={editCart} />
                 ))}
