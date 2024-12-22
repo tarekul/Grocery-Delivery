1e226127aa48573db8b7f59571eee6d3cf12eda1
tarekul <rahin6810@gmail.com>
2024-12-22 15:17:24 -0500

    replace first two images

--- client/src/components/card-item/card-item.jsx
+++ client/src/components/card-item/card-item.jsx
@@ -4,7 +4,7 @@ const CardItem = ({item, editCart, cart}) => {
     const enableButton = cart.find(cartItem => cartItem.item.id === item.id);
     return (
         <div className="card-item">
-            <img src={item.image} alt={item.name} />
+            <img src={process.env.REACT_APP_API_URL + item.image} alt={item.name} />
                 <h3 className="item-name">{item.name}</h3>
                 <p className="item-price">${item.price}</p>
             <div className="item-quantity">
