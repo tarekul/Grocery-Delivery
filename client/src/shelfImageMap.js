4f337932e16eafb417fb0501d595d06701e7e887
tarekul <rahin6810@gmail.com>
2025-08-02 05:53:56 -0400

    cache shelf images

--- client/src/shelfImageMap.js
+++ client/src/shelfImageMap.js
@@ -1,7 +1,18 @@
 import apiUrl from "./apiUrl";
 
-const shelfImageMap = {
-  dairy_shelf_1: `${apiUrl}/shelf-images/dairy-shelf-1.jpg`,
-};
+if (localStorage.getItem("shelfImageMap") === null) {
+  localStorage.setItem(
+    "shelfImageMap",
+    JSON.stringify({
+      dairy_shelf_1: `${apiUrl}/shelf-images/dairy-shelf-1.jpg`,
+    })
+  );
+}
+
+const shelfImageMap = localStorage.getItem("shelfImageMap")
+  ? JSON.parse(localStorage.getItem("shelfImageMap"))
+  : {
+      dairy_shelf_1: `${apiUrl}/shelf-images/dairy-shelf-1.jpg`,
+    };
 
 export default shelfImageMap;
