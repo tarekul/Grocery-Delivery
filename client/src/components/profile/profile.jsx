2cd79aba0bc8dbdab42be14197e8648ca0d8d720
Sangram Mathews <mathews0171@gmail.com>
2025-08-27 21:11:47 -0400

    Added Code on profile jsx

--- client/src/components/profile/profile.jsx
+++ client/src/components/profile/profile.jsx
@@ -1,20 +1,41 @@
+import React, { useState } from "react";
+import { getUser } from "../../functions/getUser";
+import { useAuth } from "../../contexts/authContext";
 const Profile = () => {
-  return (
-    <div>
-      <h1>User Profile</h1>
-      <h2>Welcome Sangram Mathews</h2>
-      <p>Your Email: mathews0171@gmail.com</p>
-      <p>Your Phone: 6467644467</p>
-      <p>Your Address: 8907 89th ave</p>
-      <p>Your City: Queens</p>
-      <p>Your State: New York</p>
-      <p>Your Zipcode: 11373</p>
+    const [user, setUser] = useState(null);
+    const { firebaseUser } = useAuth();
+    React.useEffect(() => {
+        if (firebaseUser) {
+            getUser(firebaseUser.uid)
+                .then((data) => {
+                    setUser(data);
+                })
+                .catch((err) => {
+                    console.error("Error fetching user data:", err);
+                });
+        }
+    }, [firebaseUser]);
+    console.log("Firebase User in Profile:", firebaseUser.uid);
+    if (!user) {
+        return <div>Loading...</div>;
+    } else { 
+        return (
+            <div>
+            <h1>User Profile</h1>
+            <h2>Welcome {getUser().name}</h2>
+            <p>Your Email: {getUser().email}</p>
+            <p>Your Phone: {getUser().phone}</p>
+            <p>Your Address: {getUser().address}</p>
+            <p>Your City: {getUser().city}</p>
+            <p>Your State: {getUser().state}</p>
+            <p>Your Zipcode: {getUser().zipcode}</p>
 
-      <button>Edit Profile</button>
-      
-      <button>Save Changes</button>
-    </div>
-  );
+            <button>Edit Profile</button>
+            
+            <button>Save Changes</button>
+            </div>
+        ) 
+    }
 };
 
 export default Profile;
