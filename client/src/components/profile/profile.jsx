e08879bb9bebdc103ccb1a5951482c6369fa263d
tarekul <rahin6810@gmail.com>
2025-08-27 21:43:46 -0400

    use user data in profile component

--- client/src/components/profile/profile.jsx
+++ client/src/components/profile/profile.jsx
@@ -1,41 +1,42 @@
 import React, { useState } from "react";
-import { getUser } from "../../functions/getUser";
 import { useAuth } from "../../contexts/authContext";
+import { getUser } from "../../functions/getUser";
 const Profile = () => {
-    const [user, setUser] = useState(null);
-    const { firebaseUser } = useAuth();
-    React.useEffect(() => {
-        if (firebaseUser) {
-            getUser(firebaseUser.uid)
-                .then((data) => {
-                    setUser(data);
-                })
-                .catch((err) => {
-                    console.error("Error fetching user data:", err);
-                });
-        }
-    }, [firebaseUser]);
-    console.log("Firebase User in Profile:", firebaseUser.uid);
-    if (!user) {
-        return <div>Loading...</div>;
-    } else { 
-        return (
-            <div>
-            <h1>User Profile</h1>
-            <h2>Welcome {getUser().name}</h2>
-            <p>Your Email: {getUser().email}</p>
-            <p>Your Phone: {getUser().phone}</p>
-            <p>Your Address: {getUser().address}</p>
-            <p>Your City: {getUser().city}</p>
-            <p>Your State: {getUser().state}</p>
-            <p>Your Zipcode: {getUser().zipcode}</p>
-
-            <button>Edit Profile</button>
-            
-            <button>Save Changes</button>
-            </div>
-        ) 
+  const [user, setUser] = useState(null);
+  const { firebaseUser } = useAuth();
+  React.useEffect(() => {
+    if (firebaseUser) {
+      getUser(firebaseUser.uid)
+        .then((data) => {
+          setUser(data);
+        })
+        .catch((err) => {
+          console.error("Error fetching user data:", err);
+        });
     }
+  }, [firebaseUser]);
+  if (!user) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div>
+        <h1>User Profile</h1>
+        <h2>
+          Welcome {user.firstName} {user.lastName}
+        </h2>
+        <p>Your Email: {user.email}</p>
+        <p>Your Phone: {user.phone}</p>
+        <p>Your Address: {user.address}</p>
+        <p>Your City: {user.city}</p>
+        <p>Your State: NY</p>
+        <p>Your Zipcode: {user.zipcode}</p>
+
+        <button>Edit Profile</button>
+
+        <button>Save Changes</button>
+      </div>
+    );
+  }
 };
 
 export default Profile;
